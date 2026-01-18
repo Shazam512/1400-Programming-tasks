f7ea1ca7ba4a6429d67e4e78ca511388a6c4e519
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 19:58:36 +0300

    Add files via upload

---
+++ 1.18/Program.cs
@@ -0,0 +1,24 @@
+﻿double s;
+double k = 10;
+
+// а)
+s = 5;
+s = 57;
+Console.WriteLine($"a) s = {s}");
+
+// б)
+s = 6;
+s = -5.2 * s;
+s = 0;
+Console.WriteLine($"б) s = {s}");
+
+// в)
+s = -7.5;
+s = 2 * s;
+Console.WriteLine($"в) s = {s}");
+
+// г)
+s = 45;
+s = -25;
+s = s + k;
+Console.WriteLine($"г) s = {s} (при k={k})");
