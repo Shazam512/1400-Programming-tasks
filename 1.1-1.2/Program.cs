7aa555a889c789822d1075acb9c647d73003c5cf
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 16:20:28 +0300

    Update Program.cs

--- 1.1-1.2/Program.cs
+++ 1.1-1.2/Program.cs
@@ -1,13 +1,2 @@
-﻿using System;
-
-namespace _1
-{
-    class Program
-    {
-        static void Main()
-        {
-            System.Console.WriteLine($"{Math.PI:F2}");
-            System.Console.WriteLine($"{Math.E:F1}");
-        }
-    }
-}
+System.Console.WriteLine($"{Math.PI:F2}");
+System.Console.WriteLine($"{Math.E:F1}");
