25e95e59c8b5852da0d3fdf57010f7fbd9f4ec13
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 16:57:43 +0300

    Add files via upload

---
+++ 1.14/Program.cs
@@ -0,0 +1,35 @@
+﻿// а) 2x
+double resA = 2 * x;
+
+// б) sin x
+double resB = Math.Sin(x);
+
+// в) a^2
+double resV = Math.Pow(a, 2); // или a * a
+
+// г) sqrt(x)
+double resG = Math.Sqrt(x);
+
+// д) |n|
+double resD = Math.Abs(n);
+
+// е) 5 cos y
+double resE = 5 * Math.Cos(y);
+
+// ж) -7.5 a^2
+double resJ = -7.5 * Math.Pow(a, 2);
+
+// з) 3 sqrt(x)
+double resZ = 3 * Math.Sqrt(x);
+
+// и) sin α cos β + cos α sin β
+double resI = Math.Sin(alpha) * Math.Cos(beta) + Math.Cos(alpha) * Math.Sin(beta);
+
+// к) a * sqrt(2b)
+double resK = a * Math.Sqrt(2 * b);
+
+// л) 3 sin 2α cos 3β
+double resL = 3 * Math.Sin(2 * alpha) * Math.Cos(3 * beta);
+
+// м) -5 sqrt(x + sqrt(y))
+double resM = -5 * Math.Sqrt(x + Math.Sqrt(y));
