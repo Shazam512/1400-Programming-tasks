32ddc15b8050287fe63dc926d21e1d7a71d21688
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 16:44:14 +0300

    Add files via upload

---
+++ 1.5-1.11/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1.5
+System.Console.WriteLine("1 13 49");
+// 1.6
+System.Console.WriteLine("7  15  100");
+// 1.7
+System.Console.WriteLine("6  25  80");
+// 1.8
+System.Console.WriteLine("1 5 95 46");
+// 1.9
+System.Console.WriteLine("50\n10");
+// 1.10
+System.Console.WriteLine("5\n10\n21");
+// 1.11
+System.Console.WriteLine("1\n13\n45\n75");
