208fae4c0daaff81ee8a46c8fe229d3d837d599e
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 21:40:11 +0300

    Add files via upload

---
+++ 1.24/Program.cs
@@ -0,0 +1,11 @@
+﻿// а)
+System.Console.Write("Введите а: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double x = Math.Sqrt((2 * a + Math.Sin(Math.Abs(3 * a))) / 3.56);
+System.Console.WriteLine($"x = {x}");
+
+// б)
+System.Console.Write("Введите x: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+double y = Math.Sin((3.2 + Math.Sqrt(1 + x2)) / Math.Abs(5 * x2));
+System.Console.WriteLine($"y = {y}");
