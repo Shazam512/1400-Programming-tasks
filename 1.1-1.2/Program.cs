0a08f91bbacae3b7545fb016a92bfeb55cae0f5d
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 16:15:35 +0300

    Add files via upload

---
+++ 1.1-1.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine($"{Math.PI:F2}");
+            System.Console.WriteLine($"{Math.E:F1}");
+        }
+    }
+}
