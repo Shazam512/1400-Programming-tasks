c5bd42ad1aefe5507f912f3fa2152de49d5ec769
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 15:08:29 +0300

    Update 1.4.cs

--- 1.4.cs
+++ 1.4.cs
@@ -1,3 +1,11 @@
-System.Console.Write("Введите число: ");
-string num = Console.ReadLine();
-System.Console.WriteLine($"{num} - вот какое число вы ввели");
+using System;
+
+class Task1_4
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        string input = Console.ReadLine();
+        Console.WriteLine($"{input} - вот какое число Вы ввели");
+    }
+}
