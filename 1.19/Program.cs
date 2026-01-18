edf75195cb4eb3c2ac616843151e1c4f1efdb664
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 20:03:20 +0300

    Add files via upload

---
+++ 1.19/Program.cs
@@ -0,0 +1,24 @@
+﻿double x;
+double k = 5;
+
+// a)
+x = 10;
+x = -10;
+System.Console.WriteLine($"a) x = {x}");
+
+// б)
+x = 17.5;
+x = -2 * x;
+System.Console.WriteLine($"б) x = {x}");
+
+// в)
+x = 60;
+x = x - 1;
+x = 0;
+System.Console.WriteLine($"в) x = {x}");
+
+// г)
+x = -50;
+x = -25;
+x = x + k;
+System.Console.WriteLine($"г) x = {x} (при k = {k})");
