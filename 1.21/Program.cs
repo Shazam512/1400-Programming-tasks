583aa96cd47260c63735729ddec3aa96bad02e56
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 20:55:18 +0300

    Add files via upload

---
+++ 1.21/Program.cs
@@ -0,0 +1,15 @@
+﻿double a = 5.8, b = -7.9;
+
+// a)
+b = a;
+a = b;
+b = a;
+a = b;
+System.Console.WriteLine($"a) Результат: а = {a}, b = {b}");
+
+// б)
+a = 0;
+b = -9.99;
+b = a;
+a = b;
+System.Console.WriteLine($"б) Резальтат: a = {a}, b = {b}");
