a9390c879744c0d65c4b009e062ffa8e16e5bd63
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 16:48:13 +0300

    Add files via upload

---
+++ 1.12/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Введите t, v, x, y (через пробел): ");
+string[] parts = Console.ReadLine().Split(' ');
+int t = int.Parse(parts[0]);
+int v = int.Parse(parts[1]);
+int x = int.Parse(parts[2]);
+int y = int.Parse(parts[3]);
+
+System.Console.WriteLine("\na)");
+System.Console.WriteLine("5 10");
+System.Console.WriteLine("7 см");
+
+System.Console.WriteLine("\nб)");
+System.Console.WriteLine($"100 {t}");
+System.Console.WriteLine($"1949 {v}");
+
+System.Console.WriteLine("\nв)");
+System.Console.WriteLine($"{x} 35");
+System.Console.WriteLine($"{x} {y}");
+
