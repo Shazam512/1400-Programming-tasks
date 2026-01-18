54c06e13f1f1259a8700f661d1466a292fb283fc
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 21:23:12 +0300

    Add files via upload

---
+++ 1.22/Program.cs
@@ -0,0 +1,11 @@
+﻿// a)
+System.Console.Write("Введите х: ");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = 7 * Math.Pow(x, 2) - 3 * x + 6;
+System.Console.WriteLine($"y = {y}");
+
+// б)
+System.Console.Write("Введите y: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double x2 = 12 * Math.Pow(a, 2) + 7 * a - 16;
+System.Console.WriteLine($"x2 = {x2}");
