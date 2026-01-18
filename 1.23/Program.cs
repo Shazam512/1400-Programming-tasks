592629b89cb2af8e0ecd4c929411b7703fca0df7
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 21:30:26 +0300

    Add files via upload

---
+++ 1.23/Program.cs
@@ -0,0 +1,4 @@
+﻿System.Console.Write("Введите а: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double y = (Math.Pow(a, 2) + 10) / Math.Sqrt(Math.Pow(a, 2) + 1);
+System.Console.WriteLine($"y = {y}");
