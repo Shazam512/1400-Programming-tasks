3845a70f20126e309bd93631af52da4daad3d676
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-18 20:18:58 +0300

    Add files via upload

---
+++ 1.20/Program.cs
@@ -0,0 +1,17 @@
+﻿double s, k, d;
+
+// a)
+s = 14;
+k = -3;
+d = s + 1;
+s = d;
+k = 2 * s;
+System.Console.WriteLine($"a) s = {s}, k = {k}");
+
+// б)
+s = 0;
+k = 30;
+d = k - 5;
+k = 2 * d;
+s = k - 100;
+System.Console.WriteLine($"б) s = {s}, k = {k}");
