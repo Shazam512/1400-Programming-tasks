405e674bbc404bab5fa85c0b814c64f6fc55f7bc
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 12:46:32 +0300

    Add files via upload

---
+++ 1.32.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Население: ");
+double x = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Обьем територий: ");
+double y = Convert.ToDouble(Console.ReadLine());
+if (m > 0 && V > 0)
+{
+    double p = m / V;
+    System.Console.WriteLine($"Плотность: {p:F1}");
+}
+else
+{
+    System.Console.WriteLine("Ошибка! Значение должны быть большее НУЛЯ!");
+}
+
