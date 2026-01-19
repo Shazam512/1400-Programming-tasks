d4848eb64953ba612da7e759a0245e6ad2073d4d
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 12:58:48 +0300

    Add files via upload

---
+++ 1.34.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите коэфицент а: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+if (a == 0)
+{
+    System.Console.WriteLine("Ошибка! Значения [а] должно быть больше 0!");
+}
+else
+{
+    System.Console.Write("Введите коэфицент b: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+
+    double x = -b / a;
+    System.Console.WriteLine($"Корень уровнения х = {x:F1}");
+}
+
