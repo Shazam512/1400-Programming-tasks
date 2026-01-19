8a076f044617a8867d5570b6b5f7f57973de6533
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 12:34:54 +0300

    Add files via upload

---
+++ 1.32.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите массу: ");
+double m = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите обьем: ");
+double V = Convert.ToDouble(Console.ReadLine());
+if (m > 0 && V > 0)
+{
+    double p = m / V;
+    System.Console.WriteLine($"Плотность: {p:F1}");
+}
+else
+{
+    System.Console.WriteLine("Ошибка! Значение должны быть большее НУЛЯ!");
+}
+
