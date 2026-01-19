818940c10f989ce3078385f7d58a794ef2f8e1a9
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:37:40 +0300

    Add files via upload

---
+++ 1.39.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите первое значение: ");
+double x = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+double z = (x + (2 + y) / Math.Pow(x, 2)) / (y + 1 / Math.Sqrt(Math.Pow(x, 2) + 10));
+double q = 2.8 * Math.Sin(x) + Math.Abs(y);
+
+System.Console.WriteLine($"z = {z:F2}, q = {q:F2}");
