8149676337ed3b9918224b8810e25fff8dd1a254
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:31:09 +0300

    Add files via upload

---
+++ 1.38.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Введите основания а: ");
+double a = double.Parse(Console.ReadLine());
+
+System.Console.Write("Введите основания b: ");
+double b = double.Parse(Console.ReadLine());
+
+System.Console.Write("Введите высоту h: ");
+double h = double.Parse(Console.ReadLine());
+
+double leg = Math.Abs(a - b) / 2;
+
+double side = Math.Sqrt(h * h + leg * leg);
+
+double p = a + b + 2 * side;
+
+System.Console.WriteLine($"Периметр треугольника: {p}");
