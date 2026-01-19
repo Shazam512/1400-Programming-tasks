c5a084e299a47a12eee2103a2f98b2b16a03699b
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:20:59 +0300

    Add files via upload

---
+++ 1.36.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите внешний радиус: ");
+double R = double.Parse(Console.ReadLine());
+
+System.Console.Write("Введите внутренний радиус: ");
+double r = double.Parse(Console.ReadLine());
+
+if (R > r)
+{
+    double s = Math.PI * (Math.Pow(R, 2) - Math.Pow(r, 2));
+    System.Console.WriteLine($"Площадь кольца: {s:F2}");
+}
+else
+{
+    System.Console.WriteLine("Ошибка! Внешний радиус должен быть больше внитренного!");
+}
