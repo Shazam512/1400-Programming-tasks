1f10c9da6c43d1ab10fe76743bdfda047f049ec8
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:48:04 +0300

    Add files via upload

---
+++ 1.43.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите первое значение: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+double mod1 = Math.Abs(n1);
+double mod2 = Math.Abs(n2);
+
+double arith = (mod1 + mod2) / 2;
+double geom = Math.Sqrt(mod1 * mod2);
+
+Console.WriteLine($"Среднее арифметическое модулей: {arith}");
+Console.WriteLine($"Среднее геометрическое модулей: {geom}");
