3149bc3d9f1a0ef1a32144553c1f82e325968316
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:43:07 +0300

    Update 1.40.cs

--- 1.40.cs
+++ 1.40.cs
@@ -7,4 +7,5 @@ double b = Convert.ToDouble(Console.ReadLine());
 double x = (2 / (Math.Pow(a, 2) + 25) + b) / (Math.Sqrt(b) + (a + b) / 2);
 double y = (Math.Abs(a) + 2 * Math.Sin(b)) / (5.5 * a);
 
-System.Console.WriteLine($"z = {x:F2}, q = {y:F2}");
+
+System.Console.WriteLine($"x = {x:F2}, y = {y:F2}");
