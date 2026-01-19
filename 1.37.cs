38ddb267e6c8234adcb9f2f3c6cbdb1cd649ae29
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:24:32 +0300

    Add files via upload

---
+++ 1.37.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Введите первый катет: ");
+double leg1 = double.Parse(Console.ReadLine());
+
+System.Console.Write("Введите второй катет: ");
+double leg2 = double.Parse(Console.ReadLine());
+
+double h = Math.Sqrt(leg1 * leg1 + leg2 * leg2);
+double p = leg1 + leg2 + h;
+
+System.Console.WriteLine($"Периметр треугольника: {p}");
