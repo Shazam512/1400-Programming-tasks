011dbe6440ca53796ddefb795df44c459125c8a9
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 12:24:29 +0300

    Add files via upload

---
+++ 1.31.cs
@@ -0,0 +1,16 @@
+﻿System.Console.Write("Число 1: ");
+int a = int.Parse(Console.ReadLine());
+System.Console.Write("Число 2: ");
+int b = int.Parse(Console.ReadLine());
+
+// a)
+double arif = (a + b) / 2;
+System.Console.WriteLine($"Среднее арифмитическое: {arif}");
+
+// б)
+double geom = 0;
+if (a * b >= 0)
+{
+    geom = Math.Sqrt(a * b);
+}
+System.Console.WriteLine($"Среднее гаометрическое: {geom:F2}");
