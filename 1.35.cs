cf8ffe68972e2a5bbb695c2ae8cd99251f0febfc
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:17:02 +0300

    Add files via upload

---
+++ 1.35.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Введите первый катет: ");
+double a = double.Parse(Console.ReadLine());
+
+System.Console.Write("Введите второй катет: ");
+double b = double.Parse(Console.ReadLine());
+
+double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+System.Console.WriteLine($"Гипотенуза равна: {c}");
