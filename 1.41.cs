f0d136820664ae82e776adcb1da6c204ae1dcb6b
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-19 20:43:37 +0300

    Add files via upload

---
+++ 1.41.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("Введите первое значение: ");
+double e = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double f = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double g = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double a = Math.Sqrt(Math.Pow(Math.Abs(e - 3 / f), 3) + g);
+double b = Math.Sin(e) + Math.Pow(Math.Cos(h), 2);
+double c = (33 * g) / (e * f - 3);
+
+System.Console.WriteLine($"a = {a:F2}, b = {b:F2}, c = {c:F2}");
