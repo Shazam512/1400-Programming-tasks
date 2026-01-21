78832ed861b56c35e461188c441cf3060be88b04
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 10:45:23 +0300

    Add files via upload

---
+++ 1.47.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите значение X1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение X2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение Y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение Y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние: {d:F2}");
