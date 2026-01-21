77220c14ea4aec716f0e38cc4f274389400a313d
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:56:57 +0300

    Add files via upload

---
+++ 1.56.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Скорость первого V1 (км/ч): ");
+double v1 = double.Parse(Console.ReadLine());
+
+Console.Write("Скорость второго V2 (км/ч): ");
+double v2 = double.Parse(Console.ReadLine());
+
+Console.Write("Начальный отрыв S (км): ");
+double s = double.Parse(Console.ReadLine());
+
+double time = 0.5;
+double distance = s + (v1 - v2) * time;
+
+Console.WriteLine($"Расстояние через 30 минут: {distance} км");
