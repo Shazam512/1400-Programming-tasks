c62ff214be11e2da3ca949c2da1395ad2a3a137f
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 09:56:01 +0300

    Add files via upload

---
+++ 1.44.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите первое значение: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double p = 2 * (a + b);
+double d = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+Console.WriteLine($"Периметор равен: {p}");
+Console.WriteLine($"Диагональ: {d:F2}");
