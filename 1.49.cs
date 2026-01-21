713c232cec72ecc8c489ca5c0e05b0e069e22b4c
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:27:11 +0300

    Add files via upload

---
+++ 1.49.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите значение A: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение B: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение угол: ");
+double angle = Convert.ToDouble(Console.ReadLine());
+
+double x = Math.Abs(a - b) / 2;
+double h = x * Math.Tan(angle * Math.PI / 180);
+double area = (a + b) / 2 * h;
+Console.WriteLine($"Площадь: {area:F2}");
