a5810065b9d7677ea1a3df4618b4f97279b5420a
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:55:19 +0300

    Add files via upload

---
+++ 1.55.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Скорость V1: ");
+double v1 = double.Parse(Console.ReadLine());
+
+Console.Write("Скорость V2: ");
+double v2 = double.Parse(Console.ReadLine());
+
+Console.Write("Расстояние S: ");
+double s = double.Parse(Console.ReadLine());
+
+double time = s / (v1 + v2);
+Console.WriteLine($"Автомобили встретятся через {time} ч.");
