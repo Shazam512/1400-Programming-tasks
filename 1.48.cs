eff595171032ac99f39855b1483533bcd76bdcd6
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 10:47:40 +0300

    Add files via upload

---
+++ 1.48.cs
@@ -0,0 +1,13 @@
+﻿System.Console.Write("Введите значение A: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение B: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите значение H: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double x = Math.Abs(a - b) / 2;
+double side = Math.Sqrt(h * h + x * x);
+double perimeter = a + b + 2 * side;
+Console.WriteLine($"Периметр: {perimeter:F2}");
