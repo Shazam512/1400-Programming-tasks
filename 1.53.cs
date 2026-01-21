836f9518d120cea27602d790adccdeb0e21f1991
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:51:36 +0300

    Add files via upload

---
+++ 1.53.cs
@@ -0,0 +1,8 @@
+﻿double monitor = 15000, caseUnit = 40000, keyboard = 1500, mouse = 800;
+double onePc = monitor + caseUnit + keyboard + mouse;
+
+Console.Write("Введите количество компьютеров (N): ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Стоимость 3 компьютеров: {onePc * 3}");
+Console.WriteLine($"Стоимость {n} компьютеров: {onePc * n}");
