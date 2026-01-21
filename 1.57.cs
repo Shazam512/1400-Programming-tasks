ccfcf55c67badaab01bdb5d3cc46b47a078e078d
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:58:12 +0300

    Add files via upload

---
+++ 1.57.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите температуру в градусах Цельсия: ");
+double celsius = double.Parse(Console.ReadLine());
+
+double fahrenheit = celsius * 1.8 + 32;
+double kelvin = celsius + 273.15;
+
+Console.WriteLine($"По Фаренгейту: {fahrenheit} °F");
+Console.WriteLine($"По Кельвину: {kelvin} K");
