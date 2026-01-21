198e8bc99d1367d1d7533e4ba433dce220739e86
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:50:28 +0300

    Add files via upload

---
+++ 1.52.cs
@@ -0,0 +1,13 @@
+﻿double priceCandy = 500, priceCookie = 300, priceApple = 150;
+
+Console.Write("кг конфет: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.Write("кг печенья: ");
+double y = double.Parse(Console.ReadLine());
+
+Console.Write("кг яблок: ");
+double z = double.Parse(Console.ReadLine());
+
+double total = (priceCandy * x) + (priceCookie * y) + (priceApple * z);
+Console.WriteLine($"Общая стоимость: {total} руб.");
