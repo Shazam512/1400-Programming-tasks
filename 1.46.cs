165cda6993b602fd15496451166c9baadb9bca93
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 10:08:07 +0300

    Add files via upload

---
+++ 1.46.cs
@@ -0,0 +1,14 @@
+﻿System.Console.Write("Введите первое значение: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите третье значение: ");
+double h = Convert.ToDouble(Console.ReadLine());
+
+double V = a * b * h;
+double S = 2 * (a + b) * h;
+
+Console.WriteLine($"Обьем: {V}");
+Console.WriteLine($"Площадь боковой поверхности: {S}");
