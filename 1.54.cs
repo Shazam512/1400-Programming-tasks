3e5751d0ed35b5243378a4d1b2ad1b5596b58015
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 15:53:09 +0300

    Add files via upload

---
+++ 1.54.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Возраст Тани (X): ");
+double x = double.Parse(Console.ReadLine());
+
+Console.Write("Возраст Мити (Y): ");
+double y = double.Parse(Console.ReadLine());
+
+double aver = (x + y) / 2.0;
+double diffX = Math.Abs(x - aver);
+double diffY = Math.Abs(y - aver);
+
+Console.WriteLine($"Средний возраст: {aver}");
+Console.WriteLine($"Отклонение Тани: {diffX}, Отклонение Мити: {diffY}");
