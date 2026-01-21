29292adcca9b4fd260057b94df602a2bd0ede45d
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-01-21 10:02:33 +0300

    Add files via upload

---
+++ 1.45.cs
@@ -0,0 +1,15 @@
+﻿System.Console.Write("Введите первое значение: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+System.Console.Write("Введите первое значение: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double sum = a + b;
+double ras = a - b;
+double pro = a * b;
+double chas = a / b;
+
+Console.WriteLine($"Сумма: {sum}");
+Console.WriteLine($"Разность: {ras}");
+Console.WriteLine($"Произведение: {pro}");
+Console.WriteLine($"Частное: {chas:F2}");
