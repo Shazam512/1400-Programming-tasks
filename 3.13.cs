620de5ce484e3bf2a29cd237f960aefa0a70d5ea
Eduard <152098432+Shazam512@users.noreply.github.com>
2026-02-02 10:20:50 +0300

    Add files via upload

---
+++ 3.13.cs
@@ -0,0 +1,18 @@
+Console.WriteLine("A B | a | b | c");
+
+for (int ai = 0; ai <= 1; ai++)
+{
+    for (int bi = 0; bi <= 1; bi++)
+    {
+        bool A = ai == 1;
+        bool B = bi == 1;
+
+        bool a = !(A && B);
+        bool b = (!A) || B;
+        bool c = A || (!B);
+
+        Console.WriteLine($"{ai} {bi} | {BoolToInt(a)} | {BoolToInt(b)} | {BoolToInt(c)}");
+    }
+}
+
+static int BoolToInt(bool v) => v ? 1 : 0;
